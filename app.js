015f7834b29860f5a22238abae6bda917d14a984
Tenzin <dhontenzin@gmail.com>
2022-01-28 20:24:38 +0100

    almost

--- app.js
+++ app.js
@@ -1,98 +1,22 @@
-// // click on start button
-// const startButton = document.querySelector("#button");
-// const statsContent = document.querySelector("#stats");
-// const input = document.querySelector("#input");
-// const textDisplay = document.querySelector("#text-display");
-// const form = document.querySelector("#form");
-// const mainContent = document.querySelector("#main-content");
-// const splitStory = story.split(" ");
-
-// const getRandomWord = () => splitStory[Math.floor(Math.random() * splitStory.length - 1)];
-// const nextWord = () => {
-//   const word = getRandomWord();
-//   state.currentWord = word;
-//   textDisplay.textContent = word;
-// };
-// const state = {
-//   currentWord: "",
-// };
-
-// function createWordSpan(color, content) {
-//   const span = document.createElement("span");
-//   const style = "background: " + color;
-//   span.setAttribute("style", style);
-//   span.textContent = content;
-//   return span;
-// }
-
-// startButton.addEventListener("click", function (e) {
-//   e.preventDefault();
-
-//   // hide the button
-//   startButton.className = "hide";
-
-//   // show the stats
-//   statsContent.classList = "";
-
-//   // show the input field
-//   input.className = "";
-//   input.focus();
-
-//   // show the word
-//   nextWord();
-
-//   // when user enter an input
-//   form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     let userInput = input.value;
-
-//     if (userInput === "") return;
-
-//     const isCorrect = userInput === state.currentWord;
-//     let span;
-//     if (isCorrect) {
-//       // if correct, add the word to the list with green background
-//       span = createWordSpan("green", userInput);
-//     } else {
-//       // if incorrect, add the word to the list with red background
-//       span = createWordSpan("red", userInput);
-//     }
-//     mainContent.appendChild(span);
-
-//     // clear user input
-//     input.value = "";
-//     // show the next word
-//     nextWord();
-//   });
-// });
-
-// start the timer
-
-// check if correct
-
-// for every sec, calc the stats
-// check if game has ended
-
-// when game ended, alert player WPM
-// refresh the page
-
 class Generator {
   constructor(story, time) {
     this.story = story;
-    this.time  = time;
+    this.time = time;
     this.state = {
-      currentWord : ""
-    }
+      currentWord: "",
+      score: 0,
+      timeElapsed: 0,
+    };
   }
 
-  //   generate random word
+  //   generate random word ------------------------------------
   randomWord() {
-    const splitted    = this.story.split(" ");
-    const randomWord  = splitted[Math.floor(Math.random() * splitted.length - 1)];
+    const splitted = this.story.split(" ");
+    const randomWord = splitted[Math.floor(Math.random() * splitted.length - 1)];
     return randomWord;
   }
 
-  // display random word in DOM
+  // display random word in DOM ----------------------------------
   nextWord() {
     const textDisplay = document.querySelector("#text-display");
     const word = this.randomWord();
@@ -101,62 +25,87 @@ class Generator {
     // console.log(word);
   }
 
-  // create a span with userInput
+  // create a span with userInput ----------------------------------
   createSpan(color, userInput) {
-    const span = document.createElement('span');
-    const style = 'background :' + color;
-    span.setAttribute('style', style);
+    const span = document.createElement("span");
+    const style = `background : ${color}; border-right: white 2px solid`;
+    span.setAttribute("style", style);
     span.textContent = userInput;
     return span;
   }
 
-  // button eventLIstener
+  // form submission --------------------------------------------
+  formHandler() {
+    let span = "";
+    // prettier-ignore
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      
+      // check if correct
+      const isCorrect = input.value === this.state.currentWord;
+      
+      if (isCorrect) {
+        span = this.createSpan("green", input.value);
+        this.state.score++;
+        } 
+        else span = this.createSpan("red", input.value);
+        
+        // clear input and append child span
+        input.value = "";
+        document.querySelector("#main-content").appendChild(span);
+        
+        // show the next word
+        this.nextWord();
+      }.bind(this)
+      );
+  }
+
+  // start timer clock --------------------------------------------
+  startClock() {
+    const timer = setInterval(() => {
+      this.state.timeElapsed++;
+      this.renderStats();
+
+      if (this.time == this.state.timeElapsed) {
+        clearInterval(timer);
+        alert("Game Over ! WPM : " + document.querySelector("#wpm-display").textContent);
+        window.location.reload();
+      }
+    }, 1000);
+  }
+
+  // render on DOM
+  renderStats() {
+    const wpm = document.querySelector("#wpm-display");
+    const score = document.querySelector("#score-display");
+    const time = document.querySelector("#time-display");
+
+    time.textContent = this.time - this.state.timeElapsed;
+    score.textContent = this.state.score;
+    wpm.textContent = (this.state.score / (this.state.timeElapsed / 60)).toFixed(2);
+  }
+
+  // button eventLIstener ----------------------#####----------------
   startButton() {
     const button = document.querySelector("#button");
     const stats = document.querySelector("#stats");
-    const input = document.querySelector('#input');
-    const form = document.querySelector('#form');
-    const wpm = document.querySelector('#wpm-display');
-    const time = document.querySelector('#time-display');
+    const input = document.querySelector("#input");
 
-    // insteat of bind
-    const self = this;
-
-    button.addEventListener("click", function () {
-      button.className  = "hide";
-      stats.className   = "";
-      input.className   = "";
-      input.focus();
-      self.nextWord();
-      let span          = "";
-      let count         = self.time;
-
-      (function timer() {
-        setInterval(() => {
-          count--;
-          time.textContent = count;
-        }, 1000);
-      })()
-
-      // form submission
-      form.addEventListener('submit', function(e) {
-        e.preventDefault();
-        // check if correct
-        const isCorrect = input.value === self.state.currentWord;
-        //if correct or not
-        if(isCorrect) span = self.createSpan('green', input.value);
-        else span = self.createSpan('red', input.value);
-
-        // clear input and append child span
-        input.value = "";
-        document.querySelector('#main-content').appendChild(span)
-
-        // show the next word
-        self.nextWord();
-      });
-    });
+    button.addEventListener(
+      "click",
+      function () {
+        button.className = "hide";
+        stats.className = "";
+        input.className = "";
+        input.focus();
+        this.nextWord();
+        this.formHandler();
+        this.startClock();
+        this.renderStats();
+      }.bind(this)
+    );
   }
 }
 
-const game = new Generator(story, 60);
-game.startButton()
+const game = new Generator(story, 20);
+game.startButton();
